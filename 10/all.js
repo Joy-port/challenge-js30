842460535bdf3cb5f0d28bcb0d90ad92d0b5b800
Joy <71120856+Joy-port@users.noreply.github.com>
2021-10-19 22:43:05 +0800

    Add files via upload

--- 10/all.js
+++ 10/all.js
@@ -1,11 +1,19 @@
 const lists = document.querySelectorAll(`.container li input[type="checkbox"]`);
+const total = document.querySelector(`.total`);
 
+let totalNum = 0;
 let lastChecked;
 
 function handleCheck(e){
-
+    
     let inBetween = false; //split between first / last box & boxes inbetween
     //check group
+    if(this.checked){
+        totalNum ++;
+    }else{
+        totalNum --;
+    };
+
     if(e.shiftKey && this.checked){
         
         lists.forEach(checkBox => {
@@ -18,6 +26,7 @@ function handleCheck(e){
 
             if(inBetween){
                 checkBox.checked = true;
+                totalNum ++ ;
             }
         })
     }
@@ -33,10 +42,15 @@ function handleCheck(e){
 
             if(inBetween){
                 checkBox.checked = false;
+                totalNum --;
             }
         })
     }
     lastChecked = this; // last = start
+
+    console.log(totalNum);
+    total.textContent = totalNum;
+
 }
 
 lists.forEach(item => item.addEventListener("click", handleCheck, false));
